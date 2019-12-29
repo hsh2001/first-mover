46e779ac28cc6ff24a154e5a7600b2d0a02dbe67
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2019-12-29 12:27:52 +0900

    Update test.pl

--- test/test.pl
+++ test/test.pl
@@ -3,6 +3,7 @@ use strict;
 
 use File::Basename qw(dirname);
 use Cwd qw(abs_path);
+
 use FirstMover dirname(dirname abs_path $0);
 
 sub add {
@@ -13,7 +14,15 @@ FirstMover
 ->new("Test add", sub {
   return add(1, 3);
 })
-->expectValue(4) # correct
+->expectValue(1 + 3) # correct
 ->expectValue(5) # warnings!
 ->expectValue(6) # warnings!
+;
+
+FirstMover
+->new("Test ref", sub {
+  return [];
+})
+->expectValue("array") # correct
+->expectValue("hash") # warnings!
 ;
